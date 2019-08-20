73f8c851df083572416170a4e8ea803e43b175f5
zefe <zeferino@relativity6.com>
2019-08-20 11:58:55 -0500

    modified query text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import { Pool, Client } from 'pg'
 
 //Connection to postgress sql
-const connectionString = 'postgresql://postgres:postgres@localhost:5432/test'
+const connectionString = 'postgresql://octopus:secret@localhost:5432/wtw-dev'
 const client = new Client({
     connectionString: connectionString,
 })
@@ -11,11 +11,11 @@ const client = new Client({
 client.connect()
 
 // creamos tabla si no existe
+/*
 const createTableText =
     `CREATE TABLE IF NOT EXISTS policies (
-        id	SERIAL PRIMARY KEY,
-        uniqueId VARCHAR(255) NOT NULL,
-        broker VARCHAR(255) NOT NULL,           
+        clientId VARCHAR(255) NOT NULL,
+        brokerShortName VARCHAR(255) NOT NULL,           
         department VARCHAR(255) NOT NULL,
         coverNumber INT NOT NULL,
         churnPropensityScore FLOAT NOT NULL,
@@ -64,7 +64,7 @@ client.query(createTableText)
         client.end();
     });
 
-
+*/
 
 // Read file 
 const results = []
@@ -74,16 +74,16 @@ fs.createReadStream('top_recommended_riskclass_high_prechurn_august_w1.csv')
     .on('end', () => {
         console.log(results);
         //save DataFile in DB
-        const queryText = `INSERT INTO policies(uniqueId, broker,department,coverNumber,churnPropensityScore,riskClassOnPreChurnPolicies,
-            businessDescription,versionNumber,effectiveDate,expiryDate,premium,totalIncome,stampDudy,fireServicesLevy,insureCharge,
-            brokerCharge,brokerAdminFeeGST,adminFee,discount,gstOnDiscount,gstOnPremium,brokerage,gstOnBrokerage,dueByClient,paidToDate,
-            subAgentIncome,thirdPartyBrokerRate,thirdPartyBrokerCommission,incomeClass,top1,top2,top3,top4,top5,top6,top7,top8,top9,top10)
+        const queryText = `INSERT INTO policies("clientId", "brokerShortName","department","coverNumber","churnPropensityScore","riskClass",
+            "businessDescription","versionNumber","effectiveDate","expiryDate","premium","totalIncome","stampDuty","fireServicesLevy","insurerCharge",
+            "brokerCharge","brokerAdminFeeGST","adminFee","discount","gstOnDiscount","gstOnPremium","brokerage","gstOnBrokerage","dueByClient","paidToDate",
+            "subAgentIncome","thirdPartyBrokerRate","thirdPartyBrokerCommission","incomeClass","top1","top2","top3","top4","top5","top6","top7","top8","top9","top10")
             VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,
             $33,$34,$35,$36,$37,$38,$39) RETURNING id`
 
         results.map((row) => (
             client.query(queryText, [
-                row.UniqueID,
+                row.ClientId,
                 row.Broker,
                 row.Department,
                 row.CoverNumber,
